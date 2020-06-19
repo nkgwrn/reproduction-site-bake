98f3fcf0e03a86a06379893d193423bd9f904143
Rino Nakagawara <nakagawara@reclick.co.jp>
2020-06-19 17:29:12 +0900

    WIP: メインビジュアルの実装

--- src/js/component/test1.js
+++ src/js/component/test1.js
@@ -1,5 +1,10 @@
 function test1() {
+  //TODO: 黄色い部分のアニメーション
   console.log("test1");
+  const openYellow = document.getElementById("js-opening");
+  window.addEventListener("load", function () {
+    openYellow.classList.add("is-active");
+  });
 }
 
 export { test1 };
