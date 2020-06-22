e9897b9d31685da9efa5d4adfcb42fbd179b6e30
Rino Nakagawara <nakagawara@reclick.co.jp>
2020-06-22 19:24:09 +0900

    philosophyエリアの実装

--- src/js/component/test2.js
+++ src/js/component/test2.js
@@ -1,5 +1,30 @@
-function test2() {
+const test2 = () => {
   console.log("test2");
-}
+  const PhilosophyPanels = document.querySelectorAll(".js-philosophy-panel");
+  console.log(PhilosophyPanels);
 
-export { test2 };
+  // window.addEventListener("click", function () {
+  //   PhilosophyPanel.classList.add("is-open");
+  // });
+
+  PhilosophyPanels.forEach((panel) => {
+    panel.addEventListener("click", () => {
+      console.log(panel);
+      panel.classList.toggle("is-open");
+    });
+  });
+
+  // function onPageLoad() {
+  //   var PhilosophyPanels = document.querySelectorAll('#MyList li');
+  //   for (i = 0; i < cols.length; i++)
+  //   {
+  //     cols[i].addEventListener('click', ListItemClick, false);
+  //   }
+  // }
+
+  // function ListItemClick(event) {
+  //   this.classList.toggle('selected');
+  // }
+};
+
+export default test2;
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import { test1 } from "./component/test1";
-import { test2 } from "./component/test2";
+import test2 from "./component/test2";
 
 test1();
 test2();
