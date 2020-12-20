d0a32832a6b97e0b1a8d8c9d6948528a756c14ba
Rino Nakagawara <nakagawara@reclick.co.jp>
2020-12-20 18:10:08 +0900

    Updates

---
+++ js/index.js
@@ -0,0 +1,11 @@
+import scrollAnim from "./component/scrollAnim";
+import philosophyPanel from "./component/philosophyPanel";
+import scrollNavigation from "./component/scrollNavigation";
+import heightAuto from "./component/heightAuto";
+import spNav from "./component/spNav";
+
+heightAuto();
+scrollAnim();
+philosophyPanel();
+scrollNavigation();
+spNav();
